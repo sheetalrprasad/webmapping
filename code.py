aa17316a5055b95d31d19f41d6c688b33ba65916
sheetalrprasad <sheetalraniprasad@gmail.com>
2020-07-16 11:38:48 +0530

    Stage1

--- code.py
+++ code.py
@@ -1,10 +1,16 @@
 import folium
-map = folium.Map(location=[38.58,-99.09],zoom_start=6, titles = "Stamen Terrain")
+import pandas as pd
+
+map = folium.Map(location=[38.58,-99.09],zoom_start=5, titles = "Stamen Terrain")
+
+df = pd.read_csv("Volcanoes.txt")
+volcano_loc=[]
+for i,row in df.iterrows():
+    volcano_loc.append([row['LAT'],row['LON']])
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[38.2,-99.1],popup="Hi I'm a Marker",icon=folium.Icon("green")))
-fg.add_child(folium.Marker(location=[37.2,-97.1],popup="Hi I'm a Marker",icon=folium.Icon("green")))
-
+for cor in volcano_loc:
+    fg.add_child(folium.Marker(location=cor,popup=str(cor),icon=folium.Icon("green")))
 map.add_child(fg)
 
 map.save("Map1.html")
